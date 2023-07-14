74c3c85e14fdf302ad6dc6f8e51857247556a19d
Luis Ramirez Guzman <luisramirezguzman@Luiss-MacBook-Air.local>
2023-07-14 00:38:20 -0600

    Dale

--- chats/admin.py
+++ chats/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Conversation, Message, Document, Payment, PaymentIntent, Profile
+from .models import Integration, Conversation, Message, Document, Payment, PaymentIntent, Profile
 
+admin.site.register(Integration)
 admin.site.register(Conversation)
 admin.site.register(Message)
 admin.site.register(Document)
